4a06fe4781f9939106d71756dda003e51a2fe607
Jonathan Barrett <j.e.barrett@gmail.com>
2014-01-17 12:40:33 +0000

    adds rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tarzan::Application.config.secret_token = 'd7595b26b1ebf0ec9b40d85e9cc492ddd7d0c2faac335f05df6eba6569c820716108a1bde25061138d4127239db1ee6a11898bcc9d03818b5e10c88ea6d53d75'
