6739407414ca830e0555eefa699530e14c1c6b65
Jonathan Barrett <j.e.barrett@gmail.com>
2014-01-17 13:28:43 +0000

    adds markdown helper method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  helper_method :markdown
+  def markdown(string)
+    RDiscount.new(string).to_html.html_safe
+  end
 end
